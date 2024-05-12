b51dfb076417425394fa58adbc954aaa4353aeaf
srivardhan <srivardhan.singh.rathore@gmail.com>
2024-05-12 13:22:43 +0530

    hi

---
+++ address_generator.py
@@ -0,0 +1,9 @@
+from bitcoinaddress import Wallet
+
+# Generate a Bitcoin address for the main network (Mainnet)
+wallet = Wallet()
+print(wallet)
+
+# Generate a Bitcoin address for the test network (Testnet)
+wallet = Wallet()
+print(wallet)
